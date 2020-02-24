e3fd351926c420bb8ce10e27d28c7ef46357382d
Julian Blasco <blascoblasco9@gmail.com>
2020-02-24 17:56:15 -0300

    Add basic logic and prompts

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,58 @@
 #!/usr/bin/env ruby
-puts 'Hello, world!'
+def show_board(board)
+  counter = 0
+  [1,2,3].each do |row|
+    print "-"*11,"\n" unless counter.zero?
+    [1,2,3].each do |c|
+      counter += 1
+      if counter % 3 == 0
+        print " #{board[counter-1]} "
+      else
+        print " #{board[counter-1]} |"
+      end
+    end
+    puts "\n"
+  end
+end
+
+def update_board(move, board, sym)
+  board[move-1] = sym
+  show_board(board)
+end
+
+def game_start
+  board = [1,2,3,4,5,6,7,8,9]
+
+  puts "Welcome to Tic-tac-toe"
+  print "\n"
+  show_board(board)
+  print "\n"
+
+  count = 0
+
+  while count < 3
+    print "Player 1 turn, please enter a number: "
+    p1_move = gets.chomp.to_i
+    print "\n"
+    update_board(p1_move, board, "X")
+    print "\n"
+    print "Player 2 turn, please enter a number: "
+    p2_move = gets.chomp.to_i
+    print "\n"
+    update_board(p2_move, board, "O")
+    print "\n"
+    count += 1
+  end
+end 
+
+game_start
+# puts "Welcome to Tic-tac-toe!"
+# (1..3).each do |n|
+#   puts "It's Player 1 turn. Please enter a number: "
+#   p1_move = gets.chomp!
+#   puts "The board gets updated to show player 1 move"
+#   puts "It's Player 2 turn. Please enter a number: "
+#   p2_move = gets.chomp!
+#   puts "The board gets updated to show player 2 move"
+# end
+# puts "Player 1 won. Game Over"
