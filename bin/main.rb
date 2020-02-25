a3f1494a7b14b39bf6aa118cab2286a170a1e070
Julian Blasco <blascoblasco9@gmail.com>
2020-02-25 17:22:23 -0300

    Change the prompts in game_start and fix rubocop offenses

--- bin/main.rb
+++ bin/main.rb
@@ -1,14 +1,21 @@
 #!/usr/bin/env ruby
+# rubocop: disable Metrics/MethodLength
+@win_cases = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+@board_choices = []
+@player1_choices = []
+@player2_choices = []
+@moves = 0
+
 def show_board(board)
   counter = 0
-  [1,2,3].each do |row|
-    print "-"*11,"\n" unless counter.zero?
-    [1,2,3].each do |c|
+  [1, 2, 3].each do
+    print '-' * 11, "\n" unless counter.zero?
+    [1, 2, 3].each do
       counter += 1
-      if counter % 3 == 0
-        print " #{board[counter-1]} "
+      if (counter % 3).zero?
+        print " #{board[counter - 1]} "
       else
-        print " #{board[counter-1]} |"
+        print " #{board[counter - 1]} |"
       end
     end
     puts "\n"
@@ -16,77 +23,91 @@ def show_board(board)
 end
 
 def update_board(move, board, sym)
-  board[move-1] = sym
+  board[move - 1] = sym
   show_board(board)
 end
-
-win_cases = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-board_choices = []
-player1_choices = []
-player2_choices = []
-moves = 0
 
 def check_move(move, player)
   valid_move = true
 
   if !(move.is_a? Integer) || (move < 1 || move > 9)
     valid_move = false
-  elsif board_choices.include?(move)
+  elsif @board_choices.include?(move)
     valid_move = false
   else
-    moves += 1
-    board_choices.push(move)
-    player == 1 ? player1_choices.push(move) : player2_choices.push(move)
+    @moves += 1
+    @board_choices.push(move)
+    player == 1 ? @player1_choices.push(move) : @player2_choices.push(move)
   end
 
   valid_move
 end
 
 def player_won?(player)
-  in_board = win_cases.select do |win_case|
-    (win_case - board_choices).empty?
+  in_board = @win_cases.select do |win_case|
+    (win_case - @board_choices).empty?
   end
 
   unless in_board.empty?
     in_board.each do |choice|
       if player == 1
-        return true if (choice - player1_choices).empty?
-      else
-        return true if (choice - player2_choices).empty?
+        return true if (choice - @player1_choices).empty?
+      elsif player == 2
+        return true if (choice - @player2_choices).empty?
       end
-      win_cases.delete(choice)
+      @win_cases.delete(choice)
     end
   end
   false
 end
 
 def tie?
-  return true if moves == 9
+  return true if @moves == 9
 end
 
 def game_start
-  board = [1,2,3,4,5,6,7,8,9]
+  board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-  puts "Welcome to Tic-tac-toe"
-  print "\n"
+  print "Welcome to Tic-tac-toe\n\n"
+
   show_board(board)
+
   print "\n"
 
-  count = 0
-
-  while count < 3
-    print "Player 1 turn, please enter a number: "
+  loop do
+    print 'Player 1 turn, please enter a number: '
     p1_move = gets.chomp.to_i
     print "\n"
-    update_board(p1_move, board, "X")
+    until check_move(p1_move, 1)
+      print 'Not a valid number, try again: '
+      p1_move = gets.chomp.to_i
+    end
+    update_board(p1_move, board, 'X')
     print "\n"
-    print "Player 2 turn, please enter a number: "
+    if player_won?(1)
+      puts "Player 1 wins!\n\n"
+      break
+    end
+    if tie?
+      print "It's a Tie!\n\n"
+      break
+    end
+    print 'Player 2 turn, please enter a number: '
     p2_move = gets.chomp.to_i
     print "\n"
-    update_board(p2_move, board, "O")
+    until check_move(p2_move, 2)
+      print 'Not a valid number, try again: '
+      p2_move = gets.chomp.to_i
+    end
+    update_board(p2_move, board, 'O')
     print "\n"
-    count += 1
+    if player_won?(2)
+      puts "Player 2 wins!\n\n"
+      break
+    end
   end
-end 
+  puts 'Game over!'
+end
 
 game_start
+# rubocop: enable Metrics/MethodLength
