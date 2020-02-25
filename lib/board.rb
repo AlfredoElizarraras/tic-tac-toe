73eb5587d31969c9f20824bb74f6a84add86da77
Julian Blasco <blascoblasco9@gmail.com>
2020-02-25 19:06:21 -0300

    Remove use of @variables and implement attr_accessors

--- lib/board.rb
+++ lib/board.rb
@@ -1,25 +1,27 @@
 class Board
+  attr_accessor :board
+
   def initialize
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
   def show_board
     counter = 0
-    board = ''
+    board_string = ''
     [1, 2, 3].each do
-      board += '-' * 11 + "\n" unless counter.zero?
+      board_string += '-' * 11 + "\n" unless counter.zero?
       [1, 2, 3].each do
         counter += 1
-        board += " #{@board[counter - 1]} " if (counter % 3).zero?
-        board += " #{@board[counter - 1]} |" unless (counter % 3).zero?
+        board_string += " #{board[counter - 1]} " if (counter % 3).zero?
+        board_string += " #{board[counter - 1]} |" unless (counter % 3).zero?
       end
-      board += "\n"
+      board_string += "\n"
     end
-    board
+    board_string
   end
 
   def update_board(move, sym)
-    @board[move - 1] = sym
+    board[move - 1] = sym
     show_board
   end
 end
--- lib/logic.rb
+++ lib/logic.rb
@@ -1,5 +1,5 @@
 class Logic
-  attr_accessor :board_choices, :player1_choices, :player2_choices, :round
+  attr_accessor :win_cases, :board_choices, :player1_choices, :player2_choices, :moves
 
   def initialize
     @win_cases = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
@@ -14,36 +14,36 @@ class Logic
 
     if !(move.is_a? Integer) || (move < 1 || move > 9)
       valid_move = false
-    elsif @board_choices.include?(move)
+    elsif board_choices.include?(move)
       valid_move = false
     else
-      @moves += 1
-      @board_choices.push(move)
-      player == 1 ? @player1_choices.push(move) : @player2_choices.push(move)
+      self.moves += 1
+      board_choices.push(move)
+      player == 1 ? player1_choices.push(move) : player2_choices.push(move)
     end
 
     valid_move
   end
 
   def player_won?(player)
-    in_board = @win_cases.select do |win_case|
+    in_board = win_cases.select do |win_case|
       (win_case - board_choices).empty?
     end
 
     unless in_board.empty?
       in_board.each do |choice|
         if player == 1
-          return true if (choice - @player1_choices).empty?
+          return true if (choice - player1_choices).empty?
         elsif player == 2
-          return true if (choice - @player2_choices).empty?
+          return true if (choice - player2_choices).empty?
         end
-        @win_cases.delete(choice)
+        win_cases.delete(choice)
       end
     end
     false
   end
 
   def tie?
-    return true if @moves == 9
+    return true if moves == 9
   end
 end
