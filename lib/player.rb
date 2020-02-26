c215d8963d6972157e7cdd3201e7436acc6d36ef
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-02-26 11:38:29 -0600

    Fix rubocop offenses on: board, error and player classes.

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,7 @@
 class Player
   attr_reader :name, :mark, :player_number
 
-  def initialize(name,mark,player_number)
+  def initialize(name, mark, player_number)
     @name = name
     @mark = mark
     @player_number = player_number
