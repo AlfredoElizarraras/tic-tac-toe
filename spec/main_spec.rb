0db5ac99cf390ffa46d911a776beee4f78b27c9e
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-03-02 09:43:28 -0600

    Fix rubocop offenses.

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -5,10 +5,11 @@ require_relative '../lib/player.rb'
 require 'stringio'
 
 RSpec.describe Main do
-  let(:instructions) { "Welcome to Tic-tac-toe\nSelect a number to replace with the given mark:\nPlayer 1: 'X'\nPlayer 2: 'O'\n\n\n" }
-  let(:player_prompt) { "Player asd turn, please enter a number: " }
-  let(:player1_name) { 'Mark' }
-  let(:player2_name) { 'Eve' }
+  let(:instructions_1) { "Welcome to Tic-tac-toe\nSelect a number to replace with the given mark:" }
+  let(:instructions) { instructions_1 + "\nPlayer 1: 'X'\nPlayer 2: 'O'\n\n\n" }
+  let(:player_prompt) { 'Player asd turn, please enter a number: ' }
+  let(:p1_name) { 'Mark' }
+  let(:p2_name) { 'Eve' }
   let(:player1_wins) { /Player\sMark\swins!\n\nGame\sover!\n/ }
   let(:player2_wins) { /Player\sEve\swins!\n\nGame\sover!\n/ }
   let(:tie) { /It\'s\sa\sTie!\n\nGame\sover!\n/ }
@@ -39,8 +40,8 @@ RSpec.describe Main do
   describe '#player_turn' do
     it 'prompts the player to make a move' do
       main = Main.new
-      player = Player.new("asd", 'X', 1)
-      allow($stdin).to receive(:gets).and_return "1"
+      player = Player.new('asd', 'X', 1)
+      allow($stdin).to receive(:gets).and_return '1'
       expect do
         main.player_turn(player)
       end.to output(player_prompt).to_stdout
@@ -51,22 +52,22 @@ RSpec.describe Main do
     main = Main.new
 
     it 'create an instance of a player and if the parameter given is true assign a name to player 1' do
-      allow($stdin).to receive(:gets).and_return player1_name
+      allow($stdin).to receive(:gets).and_return p1_name
       main.assign_names(true)
-      expect(main.player1.name).to eql(player1_name)
+      expect(main.player1.name).to eql(p1_name)
     end
 
     it 'create an instance of a player and if the parameter given is false assign a name to player 2' do
-      allow($stdin).to receive(:gets).and_return player2_name
+      allow($stdin).to receive(:gets).and_return p2_name
       main.assign_names(false)
-      expect(main.player2.name).to eql(player2_name)
+      expect(main.player2.name).to eql(p2_name)
     end
   end
 
   describe '#game_start' do
     it 'outputs that the player 1 won and a game over message' do
       main = Main.new
-      allow($stdin).to receive(:gets).and_return(player1_name, player2_name, "1", "2", "3", "4", "5", "6", "7")
+      allow($stdin).to receive(:gets).and_return(p1_name, p2_name, '1', '2', '3', '4', '5', '6', '7')
       expect do
         main.game_start
       end.to output(player1_wins).to_stdout
@@ -74,7 +75,7 @@ RSpec.describe Main do
 
     it 'outputs that the player 2 won and a game over message' do
       main = Main.new
-      allow($stdin).to receive(:gets).and_return(player1_name, player2_name, "1", "2", "3", "5", "4", "8")
+      allow($stdin).to receive(:gets).and_return(p1_name, p2_name, '1', '2', '3', '5', '4', '8')
       expect do
         main.game_start
       end.to output(player2_wins).to_stdout
@@ -82,7 +83,7 @@ RSpec.describe Main do
 
     it 'outputs that there was a tie and a game over message' do
       main = Main.new
-      allow($stdin).to receive(:gets).and_return(player1_name, player2_name, "1", "2", "3", "5", "4", "6", "9", "7", "8")
+      allow($stdin).to receive(:gets).and_return(p1_name, p2_name, '1', '2', '3', '5', '4', '6', '9', '7', '8')
       expect do
         main.game_start
       end.to output(tie).to_stdout
